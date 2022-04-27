8c0bd4248430c3f04ececa7518c7f020580de4b8
Assylzhan <a.baymuratov@kazdream.kz>
2022-04-28 00:18:37 +0600

    lesson-01 part-01 v2

--- src/app-func.js
+++ src/app-func.js
@@ -1,18 +1,13 @@
 import React from 'react'
 
-
-export function AppFn ()  {
-  return (
-    <div>
-      <header>
-        Func Component
-      </header>
-      <main>
-        Main
-      </main>
-      <footer>
-        Footer
-      </footer>
-    </div>
-  )
+export function AppFn ({items}){
+  return <>
+    { items.map((item, i) => {
+        return <div key={i}>
+          {item.name}
+          {item.children && <AppFn items={item.children}/>}
+        </div>
+      })
+    }
+  </>
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,44 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {App} from './app'
-// import {AppFn} from './app-func'
+import {AppFn} from './app-func'
 
-render(<App/>, document.getElementById('app'))
+let alphabetic = [
+  {
+    name: 'A',
+    children: [
+      {
+        name: 'AA',
+        children: null
+      }
+    ]
+  },
+  {
+    name: 'B',
+    children: [
+      {
+        name: 'BB',
+        children: [
+          {
+            name: 'BBB',
+            children: null,
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'C',
+    children: null
+  },
+]
+
+render(
+  <div className="some">
+    <h2>Hello, React</h2>
+    <hr/>
+    <hr/>
+    <hr/>
+    <div>Text</div>
+    <AppFn items={alphabetic}/>
+  </div>,
+  document.getElementById('app'))
